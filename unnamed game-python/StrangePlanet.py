4d0ad411190f8fd814218f57140a2db482b4aec9
KKL <s31627871@gmail.com>
2022-02-28 16:57:18 +0800

    StrangePlanet.py input path updated

--- unnamed game-python/StrangePlanet.py
+++ unnamed game-python/StrangePlanet.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		if sys.argv[1] == "basic":
 			from Engine import Engine
-			eng = Engine('map-basic.txt')
+			eng = Engine('input/map-basic.txt')
 			if eng != None:
 				eng.run()
 		elif sys.argv[1] == "extension":
 			from NewEngine import NewEngine
-			eng = NewEngine('map-extension.txt')
+			eng = NewEngine('input/map-extension.txt')
 			if eng != None:
 				eng.run()
 	else:
